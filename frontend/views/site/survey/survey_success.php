ce0253cc2f27e2b7900bd0113d11aeb790800337
Pooja <pooja@laxyosolutionsoft.com>
2021-03-09 12:45:18 +0530

    show contact us detials on survey success

--- frontend/views/site/survey/survey_success.php
+++ frontend/views/site/survey/survey_success.php
@@ -1,6 +1,21 @@
 <?php
  //echo $path = Yii::$app->homeUrl. '/images/emoji.jpg';
-echo "<h2>Thank you for giving your valuable time to complete the survey ! <i class='fa fa-smile-o' aria-hidden='true'></i></h2>";
+echo "<h2>Thank you for giving your valuable time to complete the survey ! <i class='fa fa-smile-o' aria-hidden='true'></i></h2><br><br>";
 
+echo "<p style='font-size:20px;'>We are building the legal fraternity on Social media to keep the lawyers updated with various research work that can benefit lawyers.
+</p>";
+
+echo "<p style='font-size:20px;'>Follow us on our social media  & Whats App : </p><br>";
+
+echo "Instagram <i class='fa fa-instagram' aria-hidden='true'></i> :  <a href='https://www.instagram.com/lawhub.legal/ ' target='_blank'>https://www.instagram.com/lawhub.legal/</a><br>";
+
+echo "Facebook <i class='fa fa-facebook' aria-hidden='true'></i> :  <a href='https://www.facebook.com/lawhub.in/ ' target='_blank'>https://www.facebook.com/lawhub.in/</a><br><br>";
+
+echo "<b>Add Us On : </b><br>";
+
+echo "WhatsApp <i class='fa fa-whatsapp' aria-hidden='true'></i> :  +91 88152 18334
+<br>";
+
+echo "Skype <i class='fa fa-skype' aria-hidden='true'></i> :  contact@skype.com <br>";
 ?>
 
